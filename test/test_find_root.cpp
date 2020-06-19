055756779993f33692c5fe059161faa197055193
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-19 16:56:52 +0200

    use parametrized tests

--- test/test_find_root.cpp
+++ test/test_find_root.cpp
@@ -9,43 +9,11 @@ constexpr double sqr(double x) noexcept { return x*x; }
 } // anonymous namespace
 
 
-TEST(test_parabola, test_1d)
-{
-   using namespace optimize::root;
-
-   const double precision = 1e-10;
-   const double xoffset = 0.0;
-   const double yoffset = 0.0;
-   const double xroot = xoffset - std::sqrt(-yoffset);
-   const Config config;
-   unsigned ncalls = 0;
-
-   const Fn f = [xoffset, yoffset, &ncalls] (const Vec& v) -> Vec {
-      ncalls++;
-      Vec y(v.size());
-      y(0) = sqr(v(0) - xoffset) + yoffset;
-      return y;
-   };
-
-   const Pred stop_crit = [precision] (const Vec& v, Scalar max_dx) -> bool {
-      return std::abs(v(0)) < precision || max_dx < precision;
-   };
-
-   Vec init(1);
-   init << 2.0;
-
-   const auto result = find_root(f, init, stop_crit, config);
-
-   ASSERT_EQ(result.found, true);
-   EXPECT_NEAR(result.y(0), 0.0, precision);
-   EXPECT_NEAR(result.x(0), xroot, 1e-5);
-   EXPECT_LT(result.iterations, config.max_iterations);
-   EXPECT_LE(ncalls, 1 + 3*result.iterations);
-   std::cout << "number of function calls: " << ncalls << std::endl;
-}
+class test_1d :public ::testing::TestWithParam<optimize::root::Fn> {
+};
 
 
-TEST(test_inv_gauss, test_1d)
+TEST_P(test_1d, OddYearsAreNotLeapYears)
 {
    using namespace optimize::root;
 
@@ -53,11 +21,11 @@ TEST(test_inv_gauss, test_1d)
    const Config config;
    unsigned ncalls = 0;
 
-   const Fn f = [&ncalls] (const Vec& v) -> Vec {
+   auto fn = GetParam();
+
+   const Fn f = [&ncalls, &fn] (const Vec& v) -> Vec {
       ncalls++;
-      Vec y(v.size());
-      y(0) = -std::exp(-v(0)*v(0)) + 0.5;
-      return y;
+      return fn(v);
    };
 
    const Pred stop_crit = [precision] (const Vec& v, Scalar max_dx) -> bool {
@@ -71,11 +39,28 @@ TEST(test_inv_gauss, test_1d)
 
    ASSERT_EQ(result.found, true);
    EXPECT_NEAR(result.y(0), 0.0, precision);
-   // EXPECT_NEAR(result.x(0), xroot, 1e-5);
    EXPECT_LT(result.iterations, config.max_iterations);
-   // EXPECT_LE(ncalls, 1 + 3*result.iterations);
    std::cout << "number of function calls: " << ncalls << std::endl;
 }
+
+
+INSTANTIATE_TEST_SUITE_P(
+        LeapYearTests,
+        test_1d,
+        ::testing::Values(
+           // parabola
+           [] (const optimize::root::Vec& v) -> optimize::root::Vec {
+              optimize::root::Vec y(v.size());
+              y(0) = sqr(v(0) - 0.0) + 0.0;
+              return y;
+           },
+           // inverse gauss
+           [] (const optimize::root::Vec& v) -> optimize::root::Vec {
+              optimize::root::Vec y(v.size());
+              y(0) = -std::exp(-v(0)*v(0)) + 0.5;
+              return y;
+           }
+        ));
 
 
 int main(int argc, char** argv)
